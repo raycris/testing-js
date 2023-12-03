db9edf451e983a576fe4f7fc6a0a1d6d4d8764f1
Raycris Maldonado <raycris97@gmail.com>
2023-12-03 14:23:54 -0400

    configurating eslint like a method that we can test/change our code

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,15 +1,15 @@
-const { sum, divide, multiply } = require("./02-math");
+const { sum, divide, multiply } = require('./02-math');
 
-test("adds 1 + 2 to equal 3", () => {
+test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test("adds 3 * 7 to equal 21", () => {
+test('adds 3 * 7 to equal 21', () => {
   const rta = multiply(3, 7);
   expect(rta).toBe(21);
 });
 
-test("shoul divide", () => {
+test('shoul divide', () => {
   expect(divide(20, 5)).toBe(4);
   expect(divide(20, 0)).toBeNull();
   expect(divide(24, 2)).toBe(12);
